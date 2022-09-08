a6dbe9bd87e4ea67abc368626096011568a0063f
hariharan <hariharan08112001@gmail.com>
2022-09-08 22:40:18 +0530

    Changelog : 1. Login class file fix and startup check added

--- app/src/main/java/test/AnggaRisky/fin/LoginActivity.java
+++ app/src/main/java/test/AnggaRisky/fin/LoginActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.EditText;
@@ -16,12 +17,22 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
     private EditText getEmail, getPassword;
     public ImageButton submit;
     private FirebaseAuth lAuth;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = lAuth.getCurrentUser();
+        if(currentUser != null){
+            reload();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,28 +56,27 @@ public class LoginActivity extends AppCompatActivity {
         String vEmail = getEmail.getText().toString();
         String vPass = getPassword.getText().toString();
 
-        if(!vEmail.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(vEmail).matches()){
-            if(!vPass.isEmpty() && vPass.length() >= 8){
-                lAuth.signInWithEmailAndPassword(vEmail, vPass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+        if(!vEmail.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(vEmail).matches()) {
+            if(!vPass.isEmpty() && vPass.length() >= 8) {
+                lAuth.signInWithEmailAndPassword(vEmail, vPass).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
-                        Toast.makeText(LoginActivity.this, "Welcome "+vEmail, Toast.LENGTH_LONG).show();
-                        Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
-                        intent.putExtra("userName", vEmail);
-                        startActivity(intent);
-                        finish();
-                    }
-                }).addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(LoginActivity.this,"Incorrect username and password", Toast.LENGTH_LONG).show();
+                        if (task.isSuccessful()) {
+                            Log.d("TAG", "signInWithEmail:success");
+                            Toast.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_LONG).show();
+                            reload();
+                        } else {
+                            Log.w("TAG", "signInWithEmail:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                        }
                     }
                 });
-            }else{
-                Toast.makeText(LoginActivity.this, "Password is incorrect", Toast.LENGTH_LONG).show();
-            }
-        }else{
-            Toast.makeText(LoginActivity.this, "Invalid Email ID", Toast.LENGTH_LONG).show();
-        }
+            }else getPassword.setError("Invalid Password");
+        }else getEmail.setError("Email is in Incorrect Format");
+    }
+
+    private void reload(){
+        Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/test/AnggaRisky/fin/MainActivity.java
+++ app/src/main/java/test/AnggaRisky/fin/MainActivity.java
@@ -8,15 +8,21 @@ import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.ImageButton;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
 
     public ImageButton loginBtn;
     public ImageButton signupBtn;
 
+    private FirebaseAuth checkAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        checkAuth = FirebaseAuth.getInstance();
 
         loginBtn = findViewById(R.id.onLogin);
         signupBtn = findViewById(R.id.onSignup);
@@ -30,5 +36,20 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(MainActivity.this, LoginActivity.class);
             startActivity(intent);
         });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = checkAuth.getCurrentUser();
+        if(currentUser != null){
+            reload();
+        }
+    }
+
+    private void reload(){
+        Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+        startActivity(intent);
     }
 }
